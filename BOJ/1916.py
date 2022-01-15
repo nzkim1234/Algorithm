d67884c41c5b3aa013a785af47a35c7165bbd923
nzkim1234 <nzkim1234@naver.com>
2022-01-15 23:49:53 +0900

    문제 풀이

--- BOJ/1916.py
+++ BOJ/1916.py
@@ -21,13 +21,13 @@ heapq.heappush(queue, [0, start -1])
 
 while queue:
     s_cost, s_node = heapq.heappop(queue)
+    
     if least_cost[s_node] < s_cost:
         continue
+
     for node, cost in graph[s_node]:
         if s_cost + cost < least_cost[node]:
             least_cost[node] = s_cost + cost
             heapq.heappush(queue, [least_cost[node], node])
-
-        
 
 print(least_cost[end - 1])
