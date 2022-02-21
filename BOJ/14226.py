57821b14d522fc3ad0622fdf62b981201a074209
nzkim1234 <nzkim1234@naver.com>
2022-02-21 23:42:49 +0900

    문제 풀이

---
+++ BOJ/14226.py
@@ -0,0 +1,4 @@
+from sys import stdin
+
+s = int(stdin.readline())
+emoji = 1
--- BOJ/16948.py
+++ BOJ/16948.py
@@ -4,7 +4,7 @@ from collections import deque
 n = int(stdin.readline())
 r1, c1, r2, c2 = map(int ,stdin.readline().split())
 queue = deque([[r1, c1], [-1, -1]])
-board = [[1e9 for _ in range(8)] for _ in range(8)]
+board = [[1e9 for _ in range(n)] for _ in range(n)]
 position = [[-2, -1], [-2, 1], [0, -2], [0, 2], [2, -1], [2, 1]]
 board[r1][c1] = 0
 count = 1
@@ -22,7 +22,7 @@ while queue:
         n_r = r + p_x
         n_c = c + p_y
 
-        if 0 <= n_r < 8 and 0 <= n_c < 8:
+        if 0 <= n_r < n and 0 <= n_c < n:
             if board[n_r][n_c] > count:
                 board[n_r][n_c] = count
                 queue.append([n_r, n_c])
