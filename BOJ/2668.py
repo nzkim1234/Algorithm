6dac12b1cda08d7a9cd17f551953b8490204bf62
nzkim1234 <nzkim1234@naver.com>
2022-03-06 23:20:13 +0900

    문제 풇이

--- BOJ/2668.py
+++ BOJ/2668.py
@@ -1,50 +1,13 @@
-from sys import stdin
+n = int(input())
+dic = {}
+for i in range(n):
+    dic[i+1] = int(input())
 
-n = int(stdin.readline())
-graph = [0]
-
-for i in range(1, n + 1):
-    graph.append(int(stdin.readline()))
-
-value = 0
-result = []
-
-for i in range(1, n + 1):
-    visit_graph = [False] * (n + 1)
-    dict = {}
-
-    for add_dict in range(n + 1):
-        dict[add_dict] = 0
-    
-    if not visit_graph[i]:
-        visit_graph[i] = True
-        value = graph[i]
-        dict[i] += 1
-        dict[value] += 1
-
-        while True:
-            if not visit_graph[value]:
-                visit_graph[value] = True
-                dict[value] += 1
-                value = graph[value]
-                dict[value] += 1
-            else:
-                break
-
-    can_be_result = []
-    can_be = True
-    for i in dict.items():
-        if i[1] % 2 == 0:
-            if i[1] != 0:
-                can_be_result.append(i[0])
-        else:
-            can_be = False
-            break
-    
-    if can_be:
-        result += can_be_result
-
-result = list(set(result))
-print(len(result))
-for i in result:
-    print(i)
+while True:
+    baseSet = set(dic.values())
+    dic = {key:value for key, value in dic.items() if key in baseSet}
+    if baseSet == set(dic.values()):
+        break
+print(len(dic))
+for key in dic.keys():
+    print(key)
