617cd2b161b88ce767621159588f7a700ddc62c3
nzkim1234 <nzkim1234@naver.com>
2022-03-12 17:39:30 +0900

    문제 풀이

--- BOJ/12865.py
+++ BOJ/12865.py
@@ -1,12 +1,24 @@
-import queue
 from sys import stdin
 
 n, k = map(int, stdin.readline().split())
-dp = [0] * (k + 1)
-queue = []
-for _ in range(n):
-    w, v = map(int ,stdin.readline().split())
-    queue.append([w, v])
+stuff = [[0, 0]]
+dp = [[0 for _ in range(k + 1)] for _ in range(n + 1)]
 
-queue.sort(reverse= True)
-print(queue)
+for _ in range(n):
+    stuff.append(list(map(int, stdin.readline().split())))
+
+for i in range(1, n + 1):
+    for j in range(1, k + 1):
+        print(i, j)
+        w = stuff[i][0]
+        v = stuff[i][1]
+        print(w, v)
+        if j < w:
+            dp[i][j] = dp[i - 1][j]
+        else:
+            dp[i][j] = max(v + dp[i - 1][j - w],  dp[i - 1][j])
+
+        for asdf in dp:
+            print(asdf)
+        print()
+print(dp[n][k])
