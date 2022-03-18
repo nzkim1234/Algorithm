ea4c542005536fe1f3db5c3034bd23c6bdab9ec8
nzkim1234 <nzkim1234@naver.com>
2022-03-18 14:57:31 +0900

    문제 풀이

---
+++ BOJ/1748.py
@@ -0,0 +1,10 @@
+from sys import stdin 
+
+n = int(stdin.readline()) 
+len_n = len(str(n)) 
+count = 0 
+
+for i in range(len_n -1): 
+    count += 9 * 10 ** i * (i + 1) 
+
+print(count + (n - 10 ** (len_n - 1) + 1) * len_n)
--- BOJ/1992.py
+++ BOJ/1992.py
@@ -1,9 +1,10 @@
 from sys import stdin
-
+from math import log2
 n = int(stdin.readline())
 
 graph = []
-
+stack = []
 for _ in range(n):
     graph.append(list(map(int, map(str, stdin.readline().strip()))))
 
+def dfs(s_x, e_x, s_y, e_y):
