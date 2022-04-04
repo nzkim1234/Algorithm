a10d197fbd08fbaf0d0b541a9d2d84a8fb500c7e
nzkim1234 <nzkim1234@naver.com>
2022-04-05 00:46:21 +0900

    문제 풀이

--- BOJ/14503.py
+++ BOJ/14503.py
@@ -1,8 +1,9 @@
+from shutil import move
 from sys import stdin
 
 n, m = map(int, stdin.readline().split())
 r, c, d = map(int, stdin.readline().split())
-direction = [[-1, 0], [0, 1], [1, 0], [-1, 0]]
+direction = [[-1, 0], [0, 1], [1, 0], [0, -1]]
 graph = []
 count = 0
 
@@ -10,32 +11,38 @@ for _ in range(n):
     graph.append(list(map(int, stdin.readline().split())))
 
 x, y = r, c
+
 while True:
-    graph[x][y] = 2
-    print(graph)
-    count += 1
+    if graph[x][y] == 0:
+        graph[x][y] = 2
+        count += 1
+    
+    moved = False
+
     for _ in range(4):
         d -= 1
+        
         if d < 0:
             d = 3
-        
+
         n_x = x + direction[d][0]
         n_y = y + direction[d][1]
 
-        if 0 <= n_x < n and 0 <= n_y < m:
-            if graph[n_x][n_y] == 0:
-                x = n_x
-                y = n_y
-                continue
-    
-    d = (d + 2) % 4
+        if graph[n_x][n_y] == 0:
+            x = n_x
+            y = n_y
+            moved = True
+            break
 
-    n_x = x + direction[d][0]
-    n_x = y + direction[d][1]
-    print(n_x, n_y)
-    print(graph[n_x][n_y])
-    if graph[n_x][n_y] == 1:
-        break
-    else:
-        x = n_x
-        y = n_y
+    if not moved:
+        n_x = x - direction[d][0]
+        n_y = y - direction[d][1]
+
+        if graph[n_x][n_y] == 1:
+            break
+        else:
+            x = n_x
+            y = n_y
+    
+print(count)
+    
