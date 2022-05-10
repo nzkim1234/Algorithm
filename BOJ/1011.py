31683796a8db1f3c1fefbd031330e1b638a0b6f2
nzkim1234 <nzkim1234@naver.com>
2022-05-10 22:34:17 +0900

    문제 풀이

--- BOJ/1011.py
+++ BOJ/1011.py
@@ -8,6 +8,7 @@ for _ in range(t):
     count = 0  # 이동 횟수
     move = 1  # count별 이동 가능한 거리
     move_plus = 0  # 이동한 거리의 합
+    
     while move_plus < distance :
         count += 1
         move_plus += move  # count 수에 해당하는 move를 더함
