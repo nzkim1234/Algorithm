2ea0649a3994ffe6e60cb3e00b7dbdbcc2981785
nzkim1234 <nzkim1234@naver.com>
2022-05-13 22:27:48 +0900

    문제 풀이

--- BOJ/1743.py
+++ BOJ/1743.py
@@ -7,21 +7,23 @@ graph = [[0 for _ in range(m)] for _ in range(n)]
 
 for _ in range(k):
     y, x = map(int, stdin.readline().split())
-
     graph[y - 1][x - 1] = 1
 
 queue = deque()
-
 position = [[0,1],[1,0],[0,-1],[-1,0]]
 big_trash = 0
 
+# 그래프 전부 순회
 for i in range(n):
     for j in range(m):
+        
+        #쓰레기가 있을 시
         if graph[i][j] != 0:
             trash_size = 0
             graph[i][j] = 0
             queue.append([i, j])
-
+            
+            # bfs 탐색
             while queue:
                 trash_size += 1
                 y, x = queue.popleft()
@@ -29,6 +31,7 @@ for i in range(n):
                 for v_y, v_x in position:
                     n_y = y + v_y
                     n_x = x + v_x
+                    
                     if 0 <= n_y < n and 0 <= n_x < m and graph[n_y][n_x] != 0:
                         graph[n_y][n_x] = 0
                         queue.append([n_y, n_x])
--- BOJ/9465.py
+++ BOJ/9465.py
@@ -1,19 +1,21 @@
 from sys import stdin
 
 t = int(stdin.readline())
-for i in range(t):
-  s = []
-  n = int(input())
 
+for i in range(t):
+  graph = []
+  n = int(stdin.readline())
+
+    # 새로운 입력 추가
   for k in range(2):
-    s.append(list(map(int, stdin.readline().split())))
+    graph.append(list(map(int, stdin.readline().split())))
   
   for j in range(1, n):
-    if j == 1:
-      s[0][j] += s[1][j - 1]
-      s[1][j] += s[0][j - 1]
-    else:
-      s[0][j] += max(s[1][j - 1], s[1][j - 2])
-      s[1][j] += max(s[0][j - 1], s[0][j - 2])
+    if j == 1:  # 첫번째 열은 왼쪽 대각선만 더해줌
+      graph[0][j] += graph[1][j - 1]
+      graph[1][j] += graph[0][j - 1]
+    else:  # 왼쪽 대각선 또는 왼쪽 대각선의 왼쪽 인덱스 중 큰 값 더해줌
+      graph[0][j] += max(graph[1][j - 1], graph[1][j - 2])
+      graph[1][j] += max(graph[0][j - 1], graph[0][j - 2])
   
-  print(max(s[0][n - 1], s[1][n - 1]))
+  print(max(graph[0][n - 1], graph[1][n - 1]))
