95454813b6f7ed32819a260e27c317216a72a19d
nzkim1234 <nzkim1234@naver.com>
2022-05-13 22:20:18 +0900

    문제 풀이

---
+++ BOJ/9465.py
@@ -0,0 +1,19 @@
+from sys import stdin
+
+t = int(stdin.readline())
+for i in range(t):
+  s = []
+  n = int(input())
+
+  for k in range(2):
+    s.append(list(map(int, stdin.readline().split())))
+  
+  for j in range(1, n):
+    if j == 1:
+      s[0][j] += s[1][j - 1]
+      s[1][j] += s[0][j - 1]
+    else:
+      s[0][j] += max(s[1][j - 1], s[1][j - 2])
+      s[1][j] += max(s[0][j - 1], s[0][j - 2])
+  
+  print(max(s[0][n - 1], s[1][n - 1]))
