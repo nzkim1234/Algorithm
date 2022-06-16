806689d6b534cd5175f4a21298a11a83a307bd20
nzkim1234 <nzkim1234@naver.com>
2022-06-16 16:20:23 +0900

    문제 풀이

---
+++ BOJ/1043.py
@@ -0,0 +1,11 @@
+from lib2to3.pgen2 import grammar
+from sys import stdin
+
+n, m = map(int, stdin.readline().split())
+true_num = list(map(int ,stdin.readline().split()))
+graph = [[] for _ in range(m + 1)]
+for _ in range(m):
+    case = list(map(int, stdin.readline().split()))
+    graph[case[0]] += case[1::]
+
+print(graph)
