3dadd674f81c9dca588020c7c23fbb9a5767e6f7
nzkim1234 <nzkim1234@naver.com>
2022-06-21 22:21:42 +0900

    문제 풀이

--- BOJ/1043.py
+++ BOJ/1043.py
@@ -4,6 +4,7 @@ from sys import stdin
 n, m = map(int, stdin.readline().split())
 true_num = list(map(int ,stdin.readline().split()))
 graph = [[] for _ in range(m + 1)]
+
 for _ in range(m):
     case = list(map(int, stdin.readline().split()))
     graph[case[0]] += case[1::]
---
+++ BOJ/16928.py
@@ -0,0 +1,37 @@
+from sys import stdin
+from collections import deque
+
+n, m = map(int, stdin.readline().split())
+ladder = [False for _ in range(101)]
+snake = [False for _ in range(101)]
+graph = [0 for _ in range(101)]
+visit_graph = [False for _ in range(101)]
+
+for _ in range(n):
+    a, b = map(int ,stdin.readline().split())
+    ladder[a] = b
+
+for _ in range(m):
+    a, b = map(int ,stdin.readline().split())
+    snake[a] = b
+
+queue = deque()
+queue.append(1)
+visit_graph[1] = True
+
+while queue:
+    start = queue.popleft()
+    for dice in range(1, 7):
+        current = start + dice
+        if 1 <= current <= 100 and not visit_graph[current]:
+            if ladder[current]:
+                current = ladder[current]
+
+            if snake[current]:
+                current = snake[current]
+            
+            if not visit_graph[current]:
+                queue.append(current)
+                visit_graph[current] = True
+                graph[current] = graph[start] + 1
+print(graph[100])
