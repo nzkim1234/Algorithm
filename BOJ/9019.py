2328a363bef22eb1a50e4a4f20077ea6ebbb5953
nzkim1234 <nzkim1234@naver.com>
2022-07-11 17:18:55 +0900

    문제 풀이

--- BOJ/9019.py
+++ BOJ/9019.py
@@ -1,6 +1,42 @@
 from sys import stdin
+from collections import deque
 
 t = int(stdin.readline())
+
 for _ in range(t):
     a, b = map(int, stdin.readline().split())
-    print(a, b)
+    graph = [False for _ in range(10000)]
+    q = deque()
+    q.append(a)
+    graph[a] = " "
+
+    while q:
+        num = q.popleft()
+        
+        if num == b:
+            print(graph[num])
+            break
+
+        next_num = 2 * num % 10000
+        
+        if next_num < 10000 and not bool(graph[next_num]):
+            q.append(next_num)
+            graph[next_num] = graph[num] + 'D'
+            
+        next_num = (num - 1) % 10000
+
+        if next_num < 10000 and not graph[next_num]:
+            q.append(next_num)
+            graph[next_num] = graph[num] + 'S'
+
+        next_num = (10 * num + num // 1000) % 10000
+
+        if next_num < 10000 and not graph[next_num]:
+            q.append(next_num)
+            graph[next_num] = graph[num] + 'L'
+        
+        next_num = (num // 10 + num % 10 * 1000) % 10000
+
+        if next_num < 10000 and not graph[next_num]:
+            q.append(next_num)
+            graph[next_num] = graph[num] + 'R'
