fc223a3f02a31db3b9fa86110594b4af63c8a5ba
nzkim1234 <nzkim1234@naver.com>
2022-08-09 23:39:49 +0900

    문제 풀이

--- BOJ/3860.py
+++ BOJ/3860.py
@@ -4,14 +4,16 @@ while True:
     w, h = map(int, stdin.readline().split())
     if w == 0 and h == 0:
         break
+    graph = [[0 for _ in range(w)] for _ in range(h)]
     g = int(stdin.readline())
-    grave = []
     for _ in range(g):
-        grave.append(list(map(int,stdin.readline().split())))
+        x, y = map(int,stdin.readline().split())
+        graph[y][x] = 1
     e = int(stdin.readline())
     hole = []
     for _ in range(e):
         hole.append(list(map(int,stdin.readline().split())))
 
-    print(grave)
+    for i in graph:
+        print(i)
     print(hole)
