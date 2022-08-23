1c2424de6bf8a41f2972789e2281c1e9edd23f15
nzkim1234 <nzkim1234@naver.com>
2022-08-23 21:30:25 +0900

    문제 풀이

--- BOJ/15663.py
+++ BOJ/15663.py
@@ -2,12 +2,25 @@ from sys import stdin
 
 n,m = map(int, stdin.readline().split())
 nums = list(sorted(map(int, stdin.readline().split())))
+visited_list = [False] * n
 answer = []
 
 
-def dfs(start):
+def dfs():
+    if len(answer) == m:
+        print(*answer)
+    
+    else:
+        previous_num = 0
+
+        for i in range(n):
+            if not visited_list[i] and previous_num != nums[i]:
+                visited_list[i] = True
+                answer.append(nums[i])
+                previous_num = nums[i]
+                dfs()
+                visited_list[i] = False
+                answer.pop()
 
 
-for i in range(n):
-    answer = [nums]
-    dfs(i)
+dfs()
