761507c353465f352c08d20424f4cf768e76a49d
nzkim1234 <nzkim1234@naver.com>
2022-08-26 18:57:48 +0900

    문제 풀이

--- BOJ/3860.py
+++ BOJ/3860.py
@@ -1,67 +1,70 @@
+from sys import stdin
+
 while True:
-    w, h = map(int,input().split())
+    w, h = map(int,stdin.readline().split())
     
     if w==0 and h==0:
         break
     
-    graph = [ [0 for _ in range(h)]for _ in range(w) ]
-    hole=[[0]]
-    g = int(input())
+    hole_graph = [ [0 for _ in range(h)]for _ in range(w) ]
+    hole=[0]
+    g = int(stdin.readline())
 
     for _ in range(g):
-        x,y = map(int,input().split())
-        graph[x][y] = -1
+        x, y = map(int,stdin.readline().split())
+        hole_graph[x][y] = -1
     
-    e = int(input())
+    e = int(stdin.readline())
     
     for i in range(1,e+1):
-        x,y, hx,hy, t = map(int,input().split())    
-        graph[x][y] = i
-        hole.append([hx,hy,t])
+        in_x, in_y, out_x, out_y, value = map(int,stdin.readline().split())    
+        hole_graph[in_x][in_y] = i
+        hole.append([out_x, out_y, value])
 
-    dist = [ [1e9 for _ in range(h)] for _ in range(w) ]
-    dist[0][0]=0
-
+    graph = [ [float('inf') for _ in range(h)] for _ in range(w) ]
+    graph[0][0]=0
+    position = [[0,1], [1, 0], [0,-1], [-1, 0]]
     
     
-    def bf():
+    def dfs():
         for count in range(h*w):
             for x in range(w):
                 for y in range(h):
-                    
-                    if x==w-1 and y==h-1 or graph[x][y]==-1:
+                    if x == w - 1 and y == h - 1 or hole_graph[x][y] == -1:
                         continue
                     
-                    elif graph[x][y] >=1:
-                        outX,outY,outTime = hole[ graph[x][y] ]
+                    if hole_graph[x][y] >= 1:
+                        outX,outY,outTime = hole[hole_graph[x][y]]
 
-                        if dist[outX][outY] > dist[x][y] + outTime :
-                            dist[outX][outY] = dist[x][y] + outTime 
+                        if graph[outX][outY] > graph[x][y] + outTime :
+                            graph[outX][outY] = graph[x][y] + outTime 
 
-                            if count == h*w-1 :
-                                return -1e9
-                          
-                    elif graph[x][y]==0:
-                        for nx,ny in ([x,y+1], [x,y-1], [x+1,y], [x-1,y]):
+                            if count == h * w - 1 :
+                                return float('-inf')
 
+                    else:
+                        for p_x,p_y in position:
+                            n_x = x + p_x
+                            n_y = y + p_y
 
-
-                            if 0<=nx<w and 0<=ny<h : 
-                                if graph[nx][ny] >=0 :
-                                    if dist[nx][ny] > dist[x][y]+1:
-                                        dist[nx][ny] = dist[x][y]+1
+                            if 0<=n_x<w and 0<=n_y<h : 
+                                if hole_graph[n_x][n_y] >=0 :
+                                    if graph[n_x][n_y] > graph[x][y] + 1:
+                                        graph[n_x][n_y] = graph[x][y] + 1
                                         
-                                        if count == h*w-1:
-                                            return -1e9
-                                            # if check(nx,ny):
-                                            #     return False
+                                        if count == h * w - 1:
+                                            return float('-inf')
                                    
-        return dist[w-1][h-1]
+        return graph[w - 1][h - 1]
     
-    result = bf()
+
+    result = dfs()
+
     if result == float('-inf') :
         print('Never')
-    elif result == 1e9:
-        print('Impossible')
     else:
-        print(result)                  
+        if result == float('inf'):
+            print('Impossible')
+        
+        else:
+            print(result)                  
