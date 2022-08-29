34abccd518fe38e0a0c3a379cf20ae7bae61bbc0
nzkim1234 <nzkim1234@naver.com>
2022-08-29 19:51:54 +0900

    문제 풀이

--- BOJ/9663_1.py
+++ BOJ/9663_1.py
@@ -2,49 +2,33 @@ from sys import stdin
 
 n = int(stdin.readline())
 
-graph = [[0 for _ in range(n)] for _ in range(n)]
-answer = 0
+graph = [0] * n
+result = 0
 
 
-def is_possible(x, y):
-    for i in range(n):
-        if graph[i][y]:
+def is_possible(num):
+    for i in range(num):
+        if graph[num] == graph[i] or abs(num - i) - abs(graph[num] - graph[i]) == 0:
             return False
-    c_x = x
-    c_y = y
-    for _ in range(n):
-        c_x -= 1
-        c_y -= 1
-        if c_x < 0 or c_y < 0:
-            break
-        else:
-            if graph[c_x][c_y]:
-                return False
 
-    c_x = x
-    c_y = y
-    
-    for _ in range(n):
-        c_x -= 1
-        c_y += 1
-        if c_x < 0 or c_y > n - 1:
-            break
-        else:
-            if graph[c_x][c_y]:
-                return False
     return True
 
-def queen(x, y, queen_num):
-    global answer 
-    if queen_num == n:
-        answer += 1
-    else:    
-        for c_x in range(x, n):
-            for c_y in range(n):
-                if is_possible(c_x, c_y):
-                    graph[c_x][c_y] = 1
-                    queen(c_x + 1, c_y, queen_num + 1)
-                    graph[c_x][c_y] = 0
 
-queen(0, 0, 0)
-print(answer)
+
+def queen(num):
+    global result
+    if num == n:
+        result += 1
+        return
+    
+    for i in range(n):
+        graph[num] = i
+         
+        if is_possible(num):
+            queen(num + 1)
+
+
+
+queen(0)
+
+print(result)
