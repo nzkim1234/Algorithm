a70e41ac2c1d4d0824856f675386d62820d784fc
nzkim1234 <nzkim1234@naver.com>
2022-09-08 23:29:14 +0900

    문제 풀이

---
+++ Programmers/k진수에서_소수_개수_구하기.py
@@ -0,0 +1,20 @@
+def solution(n, k):
+    answer = 0
+    change = ""
+    while n > k:
+        change += str(n % k)
+        n = n // k
+    change += str(n % k)
+    change = "".join(list(reversed(change))).split('0')
+    graph = [True] * 1000001
+    graph[0], graph[1] = False, False
+    for i in range(2, 1000001):
+        if graph[i]:
+            for j in range(i + i, 1000001, i):
+                
+                graph[j] = False
+    for i in change:
+        if i != '':
+            if graph[int(i)]:
+                answer += 1
+    return answer
