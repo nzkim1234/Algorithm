6724111c9b39ef6def425bf45a985bb7a5955c0e
nzkim1234 <nzkim1234@naver.com>
2022-10-11 13:24:51 +0900

    문제 풀이

--- BOJ/14002.py
+++ BOJ/14002.py
@@ -2,6 +2,20 @@ from sys import stdin
 
 n = int(stdin.readline())
 a = list(map(int,stdin.readline().split()))
+dp = [1] * n
+result = []
 
-print(n)
-print(a)
+for i in range(1, n):
+    for j in range(0, i):
+        if a[i] > a[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+max_len = max(dp)
+
+for i in range(n -1, -1, -1):
+    if dp[i] == max_len:
+        result.append(a[i])
+        max_len -= 1
+
+print(max(dp))
+print(*reversed(result))
